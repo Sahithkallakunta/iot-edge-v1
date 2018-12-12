646182f6d959a0be4605119eedfb5cfd23fe7c47
Sahithkallakunta <sahithkallakunta@gmail.com>
2018-12-11 16:04:14 -0800

    Update traceability.cmd

--- v1/jenkins/traceability.cmd
+++ v1/jenkins/traceability.cmd
@@ -1,4 +1,5 @@
 @REM Copyright (c) Microsoft. All rights reserved.
 @REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-C:\traceability_tool\traceabilitytool.exe -i "$(Agent.BuildDirectory)\s\v1" -buildcheck -e "$(Agent.BuildDirectory)\s\v1\deps;$(Agent.BuildDirectory)\s\v1\bindings\nodejs;$(Agent.BuildDirectory)\s\v1\bindings\dotnetcore\dotnet-core-binding"if errorlevel 1 goto :eof
+C:\traceability_tool\traceabilitytool.exe -i "$(Agent.BuildDirectory)\s\v1" -buildcheck -e "$(Agent.BuildDirectory)\s\v1\deps;$(Agent.BuildDirectory)\s\v1\bindings\nodejs;$(Agent.BuildDirectory)\s\v1\bindings\dotnetcore\dotnet-core-binding"
+if errorlevel 1 goto :eof
